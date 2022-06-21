27d1d62a212725288008af4bc109e735f4baba52
miguelknals <37269261+miguelknals@users.noreply.github.com>
2022-06-21 20:01:49 +0200

    Add files via upload

--- build_dataset.py
+++ build_dataset.py
@@ -6,6 +6,8 @@ import numpy as np
 import scipy.sparse as sp
 from tqdm import tqdm
 
+from train import test
+
 
 def add_label_column(transaction, value):
     transaction["label"] = value  # add label column with fixed value
@@ -62,26 +64,48 @@ def build_adj_mx(dims, interactions):
 
 
 class CustomerArticleDataset(torch.utils.data.Dataset):
-    def __init__(self, dataset_path, num_negatives_train=4, num_negatives_test=100, sep='\t'):
-        number_customers = 10000
+    def __init__(self, dataset_path, num_negatives_train=4, num_negatives_test=100, sep='\t'): 
+        
         column_names = ["customer_id", "article_id", "label", "t_dat"]
-        train_data = pd.read_csv(f'{dataset_path}customer.train.article', sep=sep,
-                                 header=None, names=column_names).to_numpy()
-        test_data = pd.read_csv(f'{dataset_path}customer.test.article', sep=sep,
-                                header=None, names=column_names).to_numpy()
+        # MOVIE
+        test_file_name= "movielens.test.rating"
+        train_file_name ="movielens.train.rating"
+        # H& H
+        test_file_name= "customer.test.article"
+        train_file_name ="customer.train.article"
 
+        
+        
+        
+        
+        train_data = pd.read_csv(f'{dataset_path}{train_file_name}', sep=sep,
+                                 header=None, names=column_names)
+        number_customers= train_data["customer_id"].nunique() # mcanals first find number of customers
+        train_data= train_data.to_numpy() # then convert to numpy 
+        
+        test_data = pd.read_csv(f'{dataset_path}{test_file_name}', sep=sep,
+                                header=None, names=column_names)
+        number_customers_test= test_data["customer_id"].nunique() # same for test
+        test_data= test_data.to_numpy()
+        
+        assert  number_customers == number_customers_test, \
+            f"number_customers in train {number_customers} should match in test {number_customers_test}"
+            
         # TAKE items, targets and test_items
-        self.targets = train_data[:, 2]
-        self.items = self.preprocess_items(train_data, number_customers)
-
+        self.targets = train_data[:, 2] # all array([1,...1]) for traindata
+        self.items = self.preprocess_items(train_data, number_customers) # mcanals comment [cust1 , itemi], [cust1, itemk]...[cust_n, itemx]     
+        
         # Save dimensions of max users and items and build training matrix
-        self.field_dims = np.max(self.items, axis=0) + 1  # ([ 943, 2625])
-        self.train_mat = build_adj_mx(self.field_dims[-1], self.items.copy())
+        self.field_dims = np.max(self.items, axis=0) + 1  # ([ ncustomers, totalilines])
+        self.train_mat = build_adj_mx(self.field_dims[-1], self.items.copy()) #mcanals self.field_dims[-1] last value in list -> num articles
 
-        # Generate train interactions with 4 negative samples for each positive
-        self.negative_sampling(num_negatives=num_negatives_train)
+        # Generate train interactions with 4 negative samples for each positive in test data
+        self.negative_sampling(num_negatives=num_negatives_train) # 
 
         # Build test set by passing as input the test item interactions
+        # we keep the side but for evaluation we add 99 negativess for each sample
+        # for each customer, 1st value is positive, 99 negatives # mcanals comment
+        # negatives samples as 100 increases memory need in inference fyi # mcanals comment
         self.test_set = self.build_test_set(self.preprocess_items(test_data, number_customers),
                                             num_neg_samples_test=num_negatives_test)
 
@@ -92,8 +116,8 @@ class CustomerArticleDataset(torch.utils.data.Dataset):
         return self.interactions[index]
 
     def preprocess_items(self, data, num_customers):
-        reindexed_items = data[:, :2].astype(np.int)
-        reindexed_items[:, 1] = reindexed_items[:, 1] + num_customers
+        reindexed_items = data[:, :2].astype(np.int) - 1 # -1 because ID begins from 1
+        reindexed_items[:, 1] = reindexed_items[:, 1] + num_customers # no se si va -1 laika
         return reindexed_items
 
     def negative_sampling(self, num_negatives):
@@ -136,15 +160,17 @@ class CustomerArticleDataset(torch.utils.data.Dataset):
         return test_set
 
 
-transactions = pd.read_csv("../data/transactions_ddup_2019-09-22_nart_5_ncust_20_ncustr_10000.csv")
-transactions = add_label_column(transactions, 1)
-transactions = transactions.sort_values(['customer_id', 't_dat'], ascending=[True, False])
+if __name__ == '__main__':    
+    transactions = pd.read_csv("../data/transactions_ddup_2019-09-22_nart_5_ncust_20_ncustr_10000.csv")
+    
+    transactions = add_label_column(transactions, 1)
+    transactions = transactions.sort_values(['customer_id', 't_dat'], ascending=[True, False])
 
-customer_dict, article_dict = extract_dictionary(transactions)
-test_dataset, train_dataset = generate_datasets(transactions, customer_dict, article_dict)
+    customer_dict, article_dict = extract_dictionary(transactions)
+    test_dataset, train_dataset = generate_datasets(transactions, customer_dict, article_dict)
 
-column_names = ["customer_id", "article_id", "label", "t_dat"]
-train_data = pd.DataFrame(train_dataset, columns= column_names)
-test_data = pd.DataFrame(test_dataset, columns= column_names)
-train_data.to_csv("../data/customer.train.article" , sep="\t", index=False,header=False)
-test_data.to_csv("../data/customer.test.article" , sep="\t", index=False,header=False)
+    column_names = ["customer_id", "article_id", "label", "t_dat"]
+    train_data = pd.DataFrame(train_dataset, columns= column_names)
+    test_data = pd.DataFrame(test_dataset, columns= column_names)
+    train_data.to_csv("../data/customer.train.article" , sep="\t", index=False,header=False)
+    test_data.to_csv("../data/customer.test.article" , sep="\t", index=False,header=False)
